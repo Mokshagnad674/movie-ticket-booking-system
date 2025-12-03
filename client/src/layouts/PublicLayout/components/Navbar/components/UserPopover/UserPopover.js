dc6cfe357575417ba15a266963a9b9b8f5c6bf1f
mokshagnadendla <mokshagnadendla@gmail.com>
2025-12-03 12:01:39 +0530

    Make Login button go directly to login page

--- client/src/layouts/PublicLayout/components/Navbar/components/UserPopover/UserPopover.js
+++ client/src/layouts/PublicLayout/components/Navbar/components/UserPopover/UserPopover.js
@@ -1,41 +1,11 @@
 import React from 'react';
-import Popover from '@material-ui/core/Popover';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
 export default function UserPopover(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  function handleClick(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function handleClose() {
-    setAnchorEl(null);
-  }
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-
   return (
-    <>
-      <Button aria-describedby={id} onClick={handleClick} variant="outlined" color="primary">
-        Login
-      </Button>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center'
-        }}>
-        {props.children}
-      </Popover>
-    </>
+    <Button component={Link} to="/login" variant="outlined" color="primary">
+      Login
+    </Button>
   );
 }
